63967dd759fb90176db630ee0ab4c767dbf80375
ChandaGrover <chanda.grover_phd19@ashoka.edu.in>
2024-05-17 22:54:34 +0530

    update script file

--- harmonization.sh
+++
@@ -1,3 +0,0 @@
-python source_code/bg_stylization/main_train.py --input_name "sailboat.png"
-
-python source_code/bg_stylization/harmonization.py --input_name 'sailboat.png' --ref_name "fg_stylized_imgs/Copperplateengraving+contrast-of-forms_sailboat.png" --harmonization_start_scale 8
